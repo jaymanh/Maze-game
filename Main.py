7c35fe428dd58fa6bec0b47469ed1702ebf4bb9d
jaymanh <65929600+jaymanh@users.noreply.github.com>
2023-05-30 11:03:18 +1000

    max maze size
    this maze will sometimes fail to generate if the resolution is this high but it mostly works

--- Main.py
+++ Main.py
@@ -9,14 +9,13 @@ def Main():
     backgroundcolour = (180, 30, 255)
 
     # Create a Pygame window with the specified resolution
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((1600, 1000))
 
     # Set the window title
     pygame.display.set_caption('Maze game')
 
     # Increase the recursion limit to handle larger mazes
     sys.setrecursionlimit(3000)
-
     # Call the game function from the imported engine module
     engine.game(screen, backgroundcolour)
 
